117460ef7c763da0280694668ff0421a20b526fe
Enes <mgid9133@uni.sydney.edu.au>
2024-05-04 13:58:03 +1000

    2nd node & nodeList for testing

--- Run.bat
+++ Run.bat
@@ -1,1 +1,3 @@
-start cmd /k python COMP3221_BlockchainNode.py 6000 NodeList.txt
+start cmd /k python COMP3221_BlockchainNode.py 6001 NodeList.txt
+
+start cmd /k python COMP3221_BlockchainNode.py 6002 NodeList2.txt
