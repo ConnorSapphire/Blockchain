8f68ea0c5ba8f2150b60646cb446711012aec1df
Connor Smith <71867196+ConnorSapphire@users.noreply.github.com>
2024-05-10 14:55:59 +1000

    client.py now takes in command arguments for ip and port

---
+++ Client.bat
@@ -0,0 +1,5 @@
+start cmd /k python client.py 192.168.137.1 6001
+
+start cmd /k python client.py 192.168.137.1 6002
+
+start cmd /k python client.py 192.168.137.1 6003
--- client.py
+++ client.py
@@ -1,10 +1,15 @@
 import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519
 import socket
+import sys
 
 from blockchain import make_signature, make_transaction
 from network import recv_prefixed, send_prefixed
 
-# TODO generate a private key and create a transaction using make_signature and make_transaction
+# get host and port
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+# generate a private key and create a transaction using make_signature and make_transaction
 # using the derived public key as the sender
 private_key = ed25519.Ed25519PrivateKey.generate()
 sender = private_key.public_key().public_bytes_raw().hex()
@@ -13,11 +18,11 @@ nonce = 0
 signature = make_signature(private_key, message)
 transaction = make_transaction(sender, message, nonce, signature)
 
-# TODO create a socket and connect to the blockchain node
+# create a socket and connect to the blockchain node
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.137.1', 6002))
+s.connect((host, port))
 
-# TODO send the transaction, receive the response and print it
+# send the transaction, receive the response and print it
 print(f"SENDING:\n{transaction}")
 send_prefixed(s, transaction.encode())
 try:
