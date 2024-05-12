044c54b6d6d315fd079908110865c80051af3212
Enes <mgid9133@uni.sydney.edu.au>
2024-05-12 15:24:03 +1000

    Added more test requests (Commented out)

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import cryptography.hazmat.primitives.asymmetric.ed25519 as ed25519
 import socket
 import sys
+import time
 
 from blockchain import make_signature, make_transaction
 from network import recv_prefixed, send_prefixed
@@ -13,7 +14,7 @@ port = int(sys.argv[2])
 # using the derived public key as the sender
 private_key = ed25519.Ed25519PrivateKey.generate()
 sender = private_key.public_key().public_bytes_raw().hex()
-message = "BOO"
+message = "AAAAAAAAAAAAAAA"
 nonce = 0
 signature = make_signature(private_key, message)
 transaction = make_transaction(sender, message, nonce, signature)
@@ -30,3 +31,54 @@ try:
     print(f"RESPONSE:\n{data}")
 except Exception as e:
     print(e)
+
+'''
+
+time.sleep(5)
+
+message = "AAAAAAAAAAAAAAAAAAAA"
+nonce = 1
+signature = make_signature(private_key, message)
+transaction = make_transaction(sender, message, nonce, signature)
+
+print(f"SENDING:\n{transaction}")
+send_prefixed(s, transaction.encode())
+try:
+    data = recv_prefixed(s).decode()
+    print(f"RESPONSE:\n{data}")
+except Exception as e:
+    print(e)
+
+
+time.sleep(5)
+
+message = "BBBBBBBBBBBBBBBBBB"
+nonce = 2
+signature = make_signature(private_key, message)
+transaction = make_transaction(sender, message, nonce, signature)
+
+print(f"SENDING:\n{transaction}")
+send_prefixed(s, transaction.encode())
+try:
+    data = recv_prefixed(s).decode()
+    print(f"RESPONSE:\n{data}")
+except Exception as e:
+    print(e)
+
+
+time.sleep(5)
+
+message = "FFFFFFFFFFFFFFFFFFFFFF"
+nonce = 5
+signature = make_signature(private_key, message)
+transaction = make_transaction(sender, message, nonce, signature)
+
+print(f"SENDING:\n{transaction}")
+send_prefixed(s, transaction.encode())
+try:
+    data = recv_prefixed(s).decode()
+    print(f"RESPONSE:\n{data}")
+except Exception as e:
+    print(e)
+    
+'''
